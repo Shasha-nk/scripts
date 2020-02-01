787d6519b4e931a049b428b11716bbb651af8e92
Shashank Chenamoni <chenamonix.shashank@intel.com>
2020-02-01 17:43:13 +0530

    2020-02-01: Add tmux installation script.
    - Created a directory "sh" to add shell scripts
    - Added tmux installation shell script to sh dir.
    
    Signed-off-by: Shashank <chshashankraj@gmail.com>

---
+++ sh/tmux_script.sh
@@ -0,0 +1,97 @@
+# Script for installing tmux on systems where you don't have root access.
+# Assumption       : C/C++ compiler & wget are installed.
+# Installation dir : $HOME/local/bin.
+set -x
+set -e
+
+TMUX_VERSION=3.0a
+LIBEVENT_VERSION=2.1.11-stable
+NCURSES_VERSION=6.1
+HOME=`pwd`
+
+check_proxy()
+{
+	url='www.google.com'
+	wget -q --proxy-user=test --proxy-password=test --spider $url
+	if [ $? = 1 ]
+	then
+		STATUS="PROXY : DISABLED"
+		wget=`wget --no-proxy`
+	else
+		STATUS="PROXY : ENABLED"
+		wget=`wget`
+	fi
+	echo $STATUS
+}
+
+create_dirs()
+{
+	mkdir -p $HOME/local $HOME/tmux_tmp
+	cd $HOME/tmux_tmp
+}
+
+download_req_files()
+{
+	wget https://github.com/tmux/tmux/releases/download/${TMUX_VERSION}/tmux-${TMUX_VERSION}.tar.gz
+	wget https://github.com/libevent/libevent/releases/download/release-${LIBEVENT_VERSION}/libevent-${LIBEVENT_VERSION}.tar.gz
+	wget https://ftp.gnu.org/pub/gnu/ncurses/ncurses-${NCURSES_VERSION}.tar.gz
+}
+
+install_libevents()
+{
+	tar xvzf libevent-${LIBEVENT_VERSION}.tar.gz
+	cd libevent-${LIBEVENT_VERSION}
+	./configure --prefix=$HOME/local --disable-shared
+	make
+	make install
+	cd ..
+}
+
+install_ncurses()
+{
+	tar xvzf ncurses-${NCURSES_VERSION}.tar.gz
+	cd ncurses-${NCURSES_VERSION}
+	./configure --prefix=$HOME/local
+	make 
+	make install
+	cd ..
+}
+
+install_tmux()
+{
+	tar xvzf tmux-${TMUX_VERSION}.tar.gz
+	cd tmux-${TMUX_VERSION}
+	./configure CFLAGS="-I$HOME/local/include \
+						-I$HOME/local/include/ncurses" \
+				LDFLAGS="-L$HOME/local/lib \
+						-L$HOME/local/include/ncurses \
+						-L$HOME/local/include" \
+				CPPFLAGS="-I$HOME/local/include \
+						-I$HOME/local/include/ncurses" \
+				LDFLAGS="-static -L$HOME/local/include \
+						-L$HOME/local/include/ncurses \
+						-L$HOME/local/lib"
+	make
+	make install
+	cp tmux $HOME/local/bin
+	cd ..
+}
+
+clean_tmp_dir()
+{
+	cd $HOME
+	rm -rf $HOME/tmux_tmp
+}
+
+run_tmux()
+{
+	check_proxy 2>&1 | tee log_tmux.txt
+	create_dirs 2>&1 | tee log_tmux.txt
+	download_req_files 2>&1 | tee log_tmux.txt
+	install_libevents 2>&1 | tee log_tmux.txt
+	install_ncurses 2>&1 | tee log_tmux.txt
+	install_tmux 2>&1 | tee log_tmux.txt
+	echo "$HOME/local/bin/tmux is now available. You can optionally add $HOME/local/bin to your PATH." 2>&1 | tee log_tmux.txt
+}
+
+run_tmux
