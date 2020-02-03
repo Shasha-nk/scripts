d2cc856fe5a48f9e40e83a37ffc990bd326a399e
name <email>
2020-02-03 05:31:43 -0500

    2020-02-03: Updated to old script
    Signed-off-by: Shashank <chshashankraj@gmail.com>

--- sh/install_tmux_script.sh
+++ sh/install_tmux_script.sh
@@ -1,35 +1,35 @@
+#!/usr/bin/bash
 # Script for installing tmux on systems where you don't have root access.
 # Assumption        : C/C++ compiler & wget are installed.
 # Installation dir  : $HOME/local/bin.
 # Details           : Script is copied from https://gist.github.com/ryin/3106801
 #                   : and updated acc to needs
-set -x
+
 set -e
+set -x
 
 HOME=`pwd`
 TMUX_VERSION=3.0a
 LIBEVENT_VERSION=2.1.11-stable
 NCURSES_VERSION=6.1
 
-check_proxy()
-{
-    url='www.google.com'
-    wget -q --proxy-user=test --proxy-password=test --spider $url
-    if [ $? = 1 ]
-    then
-        STATUS="PROXY : DISABLED"
-        wget=`wget --no-proxy`
-    else
-        STATUS="PROXY : ENABLED"
-        wget=`wget`
-    fi
-    echo $STATUS
-}
+wget=`which wget`
+url='www.google.com'
+wget -q --proxy-user=test --proxy-password=test --spider $url
+if [ $? = 1 ]
+then
+    STATUS="PROXY : DISABLED"
+    wget="/usr/bin/wget --no-proxy"
+else
+    STATUS="PROXY : ENABLED"
+    wget="/usr/bin/wget"
+fi
+echo $STATUS
 
 create_dirs()
 {
-    mkdir -p $HOME/local $HOME/tmux_tmp $HOME/log
-    cd $HOME/tmux_tmp
+    mkdir -p $HOME/local
+    mkdir -p $HOME/log
 }
 
 download_req_files()
@@ -41,55 +41,54 @@ download_req_files()
 
 install_libevents()
 {
+    cd $HOME
     tar xvzf libevent-${LIBEVENT_VERSION}.tar.gz
-    cd libevent-${LIBEVENT_VERSION}
+    mv libevent-${LIBEVENT_VERSION}.tar.gz $HOME/libevent-${LIBEVENT_VERSION}
+    cd $HOME/libevent-${LIBEVENT_VERSION}
     ./configure --prefix=$HOME/local --disable-shared
     make
     make install
-    cd ..
 }
 
 install_ncurses()
 {
+    cd $HOME
     tar xvzf ncurses-${NCURSES_VERSION}.tar.gz
-    cd ncurses-${NCURSES_VERSION}
+    mv ncurses-${NCURSES_VERSION}.tar.gz $HOME/ncurses-${NCURSES_VERSION}
+    cd $HOME/ncurses-${NCURSES_VERSION}
     ./configure --prefix=$HOME/local
     make 
     make install
-    cd ..
 }
 
 install_tmux()
 {
+    cd $HOME
     tar xvzf tmux-${TMUX_VERSION}.tar.gz
+    mv tmux-${TMUX_VERSION}.tar.gz tmux-${TMUX_VERSION}
     cd tmux-${TMUX_VERSION}
     ./configure CFLAGS="-I$HOME/local/include \
-                        -I$HOME/local/include/ncurses" \
-                LDFLAGS="-L$HOME/local/lib \
-                        -L$HOME/local/include/ncurses \
-                        -L$HOME/local/include" \
-                CPPFLAGS="-I$HOME/local/include \
-                        -I$HOME/local/include/ncurses" \
-                LDFLAGS="-static -L$HOME/local/include \
-                        -L$HOME/local/include/ncurses \
-                        -L$HOME/local/lib"
-    make
-    make install
+            -I$HOME/local/include/ncurses" \
+            LDFLAGS="-L$HOME/local/lib \
+            -L$HOME/local/include/ncurses \
+            -L$HOME/local/include"
+    CPPFLAGS="-I$HOME/local/include -I$HOME/local/include/ncurses" LDFLAGS="-static -L$HOME/local/include -L$HOME/local/include/ncurses -L$HOME/local/lib" make
     cp tmux $HOME/local/bin
-    cd ..
 }
 
 clean_tmp_dir()
 {
-    cd $HOME
-    rm -rf $HOME/tmux_tmp
+    echo "#REMOVING ALL DOWNLOADED FILES=========="
+    rm -rf $HOME/tmux-${TMUX_VERSION}
+    rm -rf $HOME/ncurses-${NCURSES_VERSION}
+    rm -rf $HOME/libevent-${LIBEVENT_VERSION}
+    echo "#CREATING SOFT LINK FOR TMUX"
 }
 
 run_tmux()
 {
-    check_proxy 2>&1 | tee $HOME/log/log_tmux.txt
-    create_dirs 2>&1 | tee -a $HOME/log/log_tmux.txt
-    download_req_files 2>&1 | tee -a $HOME/log/log_tmux.txt
+    create_dirs
+    download_req_files 2>&1 | tee $HOME/log/log_tmux.txt
     install_libevents 2>&1 | tee -a $HOME/log/log_tmux.txt
     install_ncurses 2>&1 | tee -a $HOME/log/log_tmux.txt
     install_tmux 2>&1 | tee -a $HOME/log/log_tmux.txt
